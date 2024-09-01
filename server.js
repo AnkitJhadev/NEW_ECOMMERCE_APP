a64c2872180c6a7066c9f9e28ff9cbf9d71491f1
AnkitJhadev <jha80250@gmail.com>
2024-09-01 12:11:44 +0530

    "Created a basic express server"

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
-const express =  require('express');
-const dotenv = require('dotenv')
+require('dotenv').config(); // Load environment variables from .env file
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 const app = express();
-dotenv.config();
-const PORT = process.env.PORT || 8080
+app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-    res.json({
-        "msg" : "Hello i m Starting a Server"
-    })
-})
+// Use environment variables from process.env
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL; // Access the MONGO_URI from the environment variables
 
-app.listen(PORT,()=>{
-    console.log("server is connected")
+app.use(cors({
+  origin : 'http://localhost:5173/',
+  methods : ['GET','PUT','POST',"DELETE"],
+  allowedHeaders : [
+    "Content-Type",
+    "Authorization",
+    "Cache-Control",
+    "Expires",
+    "Pragma"
+  ],
+  credentials : true
+
+}))
+
+console.log('MONGO_URL:', MONGO_URL); // Debugging line to check MONGO_URI
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
